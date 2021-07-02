a1d42a249182435023ffbcbbf2f284d7330f23e9
bcdat240601 <78672893+bcdat240601@users.noreply.github.com>
2021-07-02 12:07:26 +0700

    update code

--- storage/framework/views/202d7b25e69848f47829e22a5bdd0023725886a3.php
+++
@@ -1,8 +0,0 @@
-<form action= "<?php echo e(asset('dangki1')); ?>" method="GET">
-<?php echo csrf_field(); ?>
-UserName:<input type = "text" name="UserName"> <br>
-Password:<input type = "text" name="Password"> <br>
-Email:<input type = "text" name="Email"> <br>
-Phone:<input type = "text" name="Phone"> <br>
-<input type="Regsister">
-</form><?php /**PATH C:\xampp\htdocs\foodorder\resources\views/dangki1.blade.php ENDPATH**/ ?>
